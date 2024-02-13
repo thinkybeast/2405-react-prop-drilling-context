eaed3ed7e63f9e3e85f9ce1236ae7e4c3047db59
Max Hawkins <max.hawkins@hey.com>
2024-02-12 22:02:44 -0800

    extract main

--- src/App.jsx
+++ src/App.jsx
@@ -15,29 +15,38 @@ const colorThemes = {
   },
 };
 
+const Main = ({ theme, onChangeTheme, ...props }) => {
+  return (
+    <main {...props}>
+      <h1>Home of the Cool</h1>
+      <button onClick={() => alert("Wao 🦥")} style={colorThemes[theme]}>
+        Official Rizz Button
+      </button>
+      <select
+        name="colorPicker"
+        id="colorPicker"
+        onChange={(e) => onChangeTheme(e.target.value)}
+      >
+        <option value={"default"}>Default</option>
+        <option value={"red"}>Red</option>
+        <option value={"Green"}>Green</option>
+      </select>
+    </main>
+  );
+};
+
 function App() {
   const [theme, setTheme] = React.useState("default");
+
+  const handleColorChange = (colorTheme) => setTheme(colorTheme);
+
   return (
     <div
       id="container"
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
     >
       <header>Welcome to Cool Site 🐨</header>
-      <main>
-        <h1>Home of the Cool</h1>
-        <button onClick={() => alert("Wao 🦥")} style={colorThemes[theme]}>
-          Official Rizz Button
-        </button>
-        <select
-          name="colorPicker"
-          id="colorPicker"
-          onChange={(e) => setTheme(e.target.value)}
-        >
-          <option value={"default"}>Default</option>
-          <option value={"red"}>Red</option>
-          <option value={"Green"}>Green</option>
-        </select>
-      </main>
+      <Main theme={theme} onChangeTheme={handleColorChange} />
       <footer>Keeping it cool since {thisYear}</footer>
     </div>
   );
