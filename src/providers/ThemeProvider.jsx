311910fc401d13db708fb7bb6406851022d0b3c4
Max Hawkins <max.hawkins@hey.com>
2024-06-09 19:28:17 -0700

    fix: green button styling

--- src/providers/ThemeProvider.jsx
+++ src/providers/ThemeProvider.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 /*
-    1. A Context object that will allow us to reference the context in child components
 
-    2. A Context Provider that will wrap the child components that want access to the context
+    1. A Context Provider that will wrap the child components that want access to the context
+
+    2. A Context object that child components can use to access the context
+
 */
-
 export const ThemeContext = React.createContext();
 
 export const ThemeProvider = ({ children }) => {
